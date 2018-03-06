bebb1a75aafcdf7b28da4178581afc5be1b8d8c5
ganangah <37045056+ganangah@users.noreply.github.com>
2018-03-07 00:08:05 +0700

    Add files via upload

---
+++ GanangAH/index.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>WELCOME TO MY BLOG</title>
+        <link rel="stylesheet" href="css/cv.css">
+        
+    </head>
+    <body>
+        
+<h1>Ganang Azis Herniwan</h1>
+  
+<img class = "profile-picture" 
+     src="images/IMG20150821141827.jpg" alt="Ini Saya Ganang Azis Herniwan" style="width:300px;height:300px;">
+<p class="details">
+  <span class="bold">Tempat/Tanggal lahir:</span>Purworejo 08.08.1996
+  <br><span class="bold">Kontak Person:</span> 081325304898
+  <br><span class="bold">Email:</span> galardo.quest@gmail.com
+</p>
+</section>
+
+<section class = "cv-body">
+<h2>Riwayat Pendidikan:</h2>
+  
+<p class = "paragraph">
+   -TK Pertiwi Wonosobo<br>
+   -SDN 5 Wonosobo <samp class="normal-font">(sampai kelas 3)</samp> <br>
+   -SDN 2 Mranti Purworejo <samp class="normal-font">(sampai lulus)</samp><br>
+   -SMP Bruderan Purworejo <br>
+   -SMK PN Jurusan Multimedia <br>
+ 
+</p>
+</section>
+
+<section class = "cv-body">
+<h2>Hoby:</h2>
+  
+<p class = "paragraph">
+  -Editing Video
+  <br>-Editing Foto/Gambar
+  <br>-Fotografi
+  <br>-Belajar Pemograman
+</p>
+</section>
+
+<section class = "cv-body">
+    
+<h2>Kegiatan :</h2>
+<p class="paragraph">
+    -Bisinis <span class="normal-font">(Kaos Tiedye)</span>
+  <br>-Belajar Pemograman
+  <br>-Editing video/foto
+  <br>-Pembuatan Video/film
+</p>
+</section>
+
+<section class = "cv-body">  
+<h2>Bahasa:</h2>
+<p class = "paragraph">
+  -Indonesia 
+  <br>-Jawa
+  <br>-English - <span class="normal-font">(Littel-Littel).</span>
+</p>
+</section>
+
+<section class = "cv-end"> 
+<h2>Sosial Media:</h2>
+  
+<nav> 
+<a class = "info-linkes" href="https://www.instagram.com/gaherniwan/">
+ <img src="images/Instagram-PNG-Image-58727.png" alt="Instagram" style="width:100px;height:100px;"> </a>
+<a class = "info-linkes" href="https://www.facebook.com/GanangAH">
+    <img src="images/1024px-F_icon.svg.png" alt="Instagram" style="width:80px;height:80px;"> </a>
+</nav>
+
+
+        
+        
+        
+    </body>
+</html>
